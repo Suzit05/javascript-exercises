c7a2b4c75ff932b8a6b0558c65b4e2e204b8d494
Suzit05 <103447157+Suzit05@users.noreply.github.com>
2023-07-16 14:34:20 +0530

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,9 +2,10 @@ const repeatString = function(string, num) {
   for(let i= 1; i<= num; i++){
     
 return string
+  }
 };
 
-  repeatString(hey, 3)
+  repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
