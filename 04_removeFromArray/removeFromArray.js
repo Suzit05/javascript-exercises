019eda1a9d69ed307b8e078e238384ec81446282
Suzit05 <103447157+Suzit05@users.noreply.github.com>
2023-07-17 14:07:47 +0530

    Update removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array[], num) {
+newArray= array[].pop('num');
 };
+
+removeFromArray([1,2,3,4],3]);
 
 // Do not edit below this line
 module.exports = removeFromArray;
